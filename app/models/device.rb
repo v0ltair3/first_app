5d9415ad11cb6abb426863aea73f3865cc5dd109
Ivo Rodrigues <ivo.rodrigues@gmail.com>
2013-07-25 00:41:52 +0100

    few more

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,9 @@
 class Device < ActiveRecord::Base
   attr_accessible :description, :user_id
+
+  belongs_to :user  
+  
+  validates :description, :length => { :maximum => 100 }
+  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :firstName, :lastName
+  has_many :devices
 end
